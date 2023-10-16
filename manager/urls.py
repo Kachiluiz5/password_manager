aec75ebe62e5f586cde2079b1deefcbe3a26adc6
Kachi luiz <samcerick5@gmail.com>
2023-10-16 14:33:45 +0100

    added the edit and delete feature on the password manager

--- manager/urls.py
+++ manager/urls.py
@@ -1,13 +1,14 @@
-# 
+
 
 from django.urls import path
 from . import views
 
-app_name = 'manager'  # Add this line if you want to namespace your app's URLs
+app_name = 'manager'
 
 urlpatterns = [
-    path('password-list/', views.password_list, name='password_list'),
+    path('password-list/', views.password_list, name='password_list'),  # Add this line
     path('', views.generate_password, name='generate_password'),
+    path('generate-password/', views.generate_password, name='generate_password'),
     path('update-password/<int:password_id>/', views.update_password, name='update_password'),
     path('delete-password/<int:password_id>/', views.delete_password, name='delete_password'),
 ]
